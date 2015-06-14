7366b6744a48c15f50c602a3559520c27628b119
Kevin Novak <knovak8@gmail.com>
2015-06-13 20:13:35 -0700

    Adding in list modified datetime, and moment.js
    moment.js NYI

--- routes/posts.js
+++ routes/posts.js
@@ -1,27 +1,35 @@
 var express = require('express')
-,	fs 		= require('fs');
+,	fs 		= require('fs')
+,	moment = require('moment');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-	fs.readFile('./output.json', function(err, data) {
-		if (err) throw err;
-
-		var postings = JSON.parse(data.toString() || '[]');
-		postings.forEach(function(post, index){
-			post.displayClass = '';
-
-			if (post.jobTitle.search(/S(enio)?r/i) > -1) {
-				post.displayClass = 'Senior';
-			}
-			else if (post.jobTitle.search(/J(unio)?r/i) > -1) {
-				post.displayClass = 'Junior';
-			}
-		})
-
-  		res.render('posts', { listings: postings });
+	fs.stat('./output.json', function(err, stats) {
 		
-	})
+		var lastModified = stats.mtime;
+
+
+		
+		fs.readFile('./output.json', function(err, data) {
+			if (err) throw err;
+
+			var postings = JSON.parse(data.toString() || '[]');
+			postings.forEach(function(post, index){
+				post.displayClass = '';
+
+				if (post.jobTitle.search(/S(enio)?r/i) > -1) {
+					post.displayClass = 'Senior';
+				}
+				else if (post.jobTitle.search(/J(unio)?r/i) > -1) {
+					post.displayClass = 'Junior';
+				}
+			})
+
+	  		res.render('posts', { listings: postings, lastMod: lastModified });
+			
+		}) // /readFile
+	}); // /stat
 
 });
 
